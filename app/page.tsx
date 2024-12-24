93d57b0314c68d9f21423d991529ac41d6e7f040
Shuvadeep Mandal <shuvadeepmandal5@gmail.com>
2024-12-24 19:25:40 +0530

    donno

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,18 @@ import CreateRoom from "@/components/shared/CreateRoom";
 import { headers } from "next/headers";
 
 export default function Home() {
-  const ip = headers().get("x-forwarded-for")?.split(",")[0] || "127.0.0.1";
+  // const ip = headers().get("x-forwarded-for")?.split(",")[0] || "127.0.0.1";
   // console.log("IP ADDRESS->", ip);
+  const headersList = headers();
+  console.dir(headersList);
+  const ip = headersList.get("x-forwarded-for")?.split(",")[0] || "127.0.0.1";
+  const userAgent = headersList.get("user-agent") || "unknown";
   return (
     <main className="w-full min-h-screen flex justify-center items-center">
       <div className="flex flex-col gap-4 items-center">
-        <p>{ip}</p>
+        <p>
+          {ip}_{userAgent}
+        </p>
         <CreateRoom host_ip={ip} />
       </div>
     </main>
